841451daba519d3356e82339ae25d52b3c7ef859
Sanchit Bansal <sanchitbansal10@gmail.com>
2022-10-28 18:56:53 +0530

    Simple Circle Class

---
+++ src/TestCircle.java
@@ -0,0 +1,8 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+
+        System.out.println("The circle has radius of "
+                + circle.getRadius() + " and area of " + circle.getArea());
+    }
+}
